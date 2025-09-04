0d14d207bdc45c148dc8475a3fefb24e8c9fbaf0
Daniel Junior <danieljunior1903@gmail.com>
2025-09-04 13:08:34 -0400

    Displaying data using lists

---
+++ src/com/sandaniel/jsf/listandtabledemo/StudentDataUtil.java
@@ -0,0 +1,35 @@
+package com.sandaniel.jsf.listandtabledemo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+@ApplicationScoped
+public class StudentDataUtil {
+	private List<Student> student;
+	
+	public StudentDataUtil() {
+		
+		loadSampleData();
+	}
+	
+	public void loadSampleData() {
+		
+		student = new ArrayList<>();
+		
+		student.add(new Student("Daniel","Junior","daniel1903@"));
+		student.add(new Student("Mary","Brown","mary2030@"));
+		student.add(new Student("John","Doe","doe3030@"));
+		
+	}
+	
+
+	public List<Student> getStudent() {
+		return student;
+	}
+	
+	
+}
